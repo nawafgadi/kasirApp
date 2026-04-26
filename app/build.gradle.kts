1a2cebbf3bddbbfe9964e9529d2a26a3735ec27c
Nawaf Gadi <nawafgadi@example.com>
2026-04-26 07:31:33 +0700

    Fix Gradle sync error and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -42,19 +42,19 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.activity)
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation(libs.mp.android.chart)
+    
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Converter GSON (untuk ubah JSON jadi Object secara otomatis)
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation(libs.retrofit.lib)
+    implementation(libs.retrofit.gson)
+    implementation(libs.androidx.lifecycle.runtime)
 
-    // Testing dependencies (Hilang sebelumnya)
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
