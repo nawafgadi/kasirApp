d595d010c29750d51a012a087ce6d9c99142223c
nawafgadi <nawaf52626@gmail.com>
2026-05-01 22:14:11 +0700

    ip nawaf

--- app/src/main/java/com/nawaf/kasirpas/api/RetrofitClient.kt
+++ app/src/main/java/com/nawaf/kasirpas/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.59.40.40:8000/api/"
+    private const val BASE_URL = "http://192.168.196.30:8000/api/"//wifi hp nawaf
 
     // Satu mesin Retrofit untuk semua
     private val retrofit: Retrofit by lazy {
